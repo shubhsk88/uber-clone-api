9047d7eb24e31e8b80265bfa401b9c9e95030702
Shubham Singh <shubham88singh@gmail.com>
2020-09-05 23:34:22 +0530

    Adding the mongodb database

--- src/api/hello/sayHello/sayHello.resolvers.js
+++ src/api/hello/sayHello/sayHello.resolvers.js
@@ -1,6 +1,16 @@
+import Kitten from '../../../models/Kitty';
+
 const resolvers = {
   Query: {
     hello: () => 'Hello World',
+  },
+  Mutation: {
+    createCat: async (_, { name }) => {
+      const cat = new Kitten({ name });
+      await cat.save();
+      console.log(cat.name);
+      return cat;
+    },
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import { GraphQLServer } from 'graphql-yoga';
+import 'dotenv/config';
 import schema from './schema';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
+mongoose.connect(process.env.MONGO_APP_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const PORT = process.env.PORT || 3000;
 const server = new GraphQLServer({ schema });
 const options = {
@@ -13,10 +19,15 @@ const options = {
   playground: '/playground',
 };
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
 server.express.use(morgan('dev'));
 server.express.use(helmet());
 server.express.use(cors());
 
-server.start(options, () =>
-  console.log(`the app is listening to port ${PORT}`)
-);
+db.once('open', function () {
+  server.start(options, () =>
+    console.log(`the app is listening to port ${PORT}`)
+  ); // we're connected!
+});
