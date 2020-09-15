3d149c90d259ad738eae33294b61f3223228c388
Shubham Singh <shubham88singh@gmail.com>
2020-09-16 05:23:48 +0530

    Adding the user model update

--- src/models/User.js
+++ src/models/User.js
@@ -14,38 +14,38 @@ const userSchema = new Schema({
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       'Please fill a valid email address',
     ],
-    verifiedEmail: {
-      type: Boolean,
-      default: false,
-    },
-    lastName: String,
-    firstName: String,
-    age: Number,
-    password: String,
-    phoneNumber: String,
-    verifiedPhoneNumber: {
-      type: Boolean,
-      default: false,
-    },
-    profilePhoto: String,
-    isDriving: {
-      type: Boolean,
-      default: false,
-    },
-    isRiding: {
-      type: Boolean,
-      default: false,
-    },
-    isTaken: {
-      type: Boolean,
-      default: false,
-    },
-    createdAt: Date,
-    updatedAt: Date,
-    lastLng: Types.Decimal128,
-    lastLat: Types.Decimal128,
-    lastOrientation: Types.Decimal128,
   },
+  verifiedEmail: {
+    type: Boolean,
+    default: false,
+  },
+  lastName: String,
+  firstName: String,
+  age: Number,
+  password: String,
+  phoneNumber: String,
+  verifiedPhoneNumber: {
+    type: Boolean,
+    default: false,
+  },
+  profilePhoto: String,
+  isDriving: {
+    type: Boolean,
+    default: false,
+  },
+  isRiding: {
+    type: Boolean,
+    default: false,
+  },
+  isTaken: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: Date,
+  updatedAt: Date,
+  lastLng: Types.Decimal128,
+  lastLat: Types.Decimal128,
+  lastOrientation: Types.Decimal128,
 });
 userSchema.virtual('fullName').get(function () {
   return this.firstName + ' ' + this.lastName;
