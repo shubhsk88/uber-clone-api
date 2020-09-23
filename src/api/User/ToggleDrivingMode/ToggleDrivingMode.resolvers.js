87ce28cec670583391cee0e67951193f9a5b85fe
Shubham Singh <shubham88singh@gmail.com>
2020-09-24 04:14:01 +0530

    Adding the toggle driving mode

---
+++ src/api/User/ToggleDrivingMode/ToggleDrivingMode.resolvers.js
@@ -0,0 +1,12 @@
+const { default: authResolvers } = require('../../../utils/authResolvers');
+
+const resolvers = {
+  Mutation: {
+    toggleDrivingMode: authResolvers(async (_, __, { req }) => {
+      const { user } = req;
+      user.isDriving = !user.isDriving;
+      user.save();
+      return { ok: true, error: null };
+    }),
+  },
+};
--- src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.js
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.js
@@ -8,6 +8,10 @@ const resolvers = {
       const { user } = req;
 
       try {
+        if (args.password !== null) {
+          user.password = args.password;
+          user.save();
+        }
         await User.findOneAndUpdate({ _id: user._id }, { ...args });
         return { ok: true, error: null };
       } catch (error) {
