ed829706d614fec93e8a9f738d1441d175f02bef
Shubham Singh <shubham88singh@gmail.com>
2020-09-23 06:49:47 +0530

    Adding the resolvers for complete email verification resolvers

---
+++ src/api/User/CompleteEmailVerification/CompleteEmailVerification.resolvers.js
@@ -0,0 +1,30 @@
+const { default: Verification } = require('../../../models/Verification');
+const { default: authResolvers } = require('../../../utils/authResolvers');
+
+const resolvers = {
+  Mutation: {
+    completeEmailVerification: authResolvers(async (_, args, { req }) => {
+      const { user } = req;
+      const { key } = args;
+      if (user.email) {
+        try {
+          const verified = await Verification.findOne({
+            key,
+            payload: user.email,
+          });
+          if (verified) {
+            user.verifiedEmail = true;
+            user.save();
+            return { ok: true, error: null };
+          } else {
+            return { ok: false, error: "Can't verify email" };
+          }
+        } catch (error) {
+          return { ok: false, error: error.messages };
+        }
+      } else {
+        return { ok: false, error: "User Email doesn't exist" };
+      }
+    }),
+  },
+};
