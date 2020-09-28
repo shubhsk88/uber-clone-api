acffba4aa9f5d814f947fcb5c37d545d6e638a3f
Shubham Singh <shubham88singh@gmail.com>
2020-09-28 08:11:50 +0530

    Adding the filter in the subscription to just get the locaiton of the user and driver

--- src/api/User/DriverSubscription/DriverSubscription.resolvers.js
+++ src/api/User/DriverSubscription/DriverSubscription.resolvers.js
@@ -1,9 +1,25 @@
+import { withFilter } from 'graphql-yoga';
+
 const resolvers = {
   Subscription: {
     driverSubscription: {
-      subscribe: (_, __, { pubSub }) => {
-        return pubSub.asyncIterator('driverUpdate');
-      },
+      subscribe: withFilter(
+        (_, __, { pubSub }) => pubSub.asyncIterator('driverUpdate'),
+        (payload, _, { context }) => {
+          const {
+            lastLng: userLastLng,
+            lastLat: userLastLat,
+          } = context.currentUser;
+          const { lastLng: driverLastLng, lastLat: driverLastLat } = payload;
+
+          return (
+            driverLastLat >= userLastLat - 0.05 &&
+            driverLastLat <= userLastLat + 0.05 &&
+            driverLastLng >= userLastLng - 0.05 &&
+            driverLastLng <= userLastLng + 0.05
+          );
+        }
+      ),
     },
   },
 };
